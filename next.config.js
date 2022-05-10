4227fa41a4ce9fbda91621ac5c37e62cf553d468
David Hilera <david.hilera@hotmail.com>
2022-05-10 05:34:48 -0500

    [feature] Integer Progesive Web App

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space', 'store.storeimages.cdn-apple.com', 'http2.mlstatic.com', 'w7.pngwing.com'],
   },
+})
+
   // env: {
   //   customKey: 'customValue'
   // },
@@ -17,4 +27,3 @@ module.exports = {
   //     }
   //   ]
   // }
-}
